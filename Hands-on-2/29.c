34fc337c7a5684af0f782302bbbd4cd99e74d122
Darshitpipariya <darshit2272000@gmail.com>
2022-09-12 14:52:28 +0530

    from 14 to  31

---
+++ Hands-on-2/29.c
@@ -0,0 +1,20 @@
+// Pipariya Darshitkumar
+// MT2022035
+// Write a program to remove the message queue.
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+int main()
+{
+    // generate unique no
+    key_t key = ftok(".", 1); // projext path is current directory and project id=1
+    // create massage queue using msgget
+    //  msgget creates a message queue
+    //  and returns identifier
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    struct msqid_ds buf;
+    //remove msg queue using flag IPC_RMID
+    msgctl(msgid, IPC_RMID, &buf);
+    return 0;
+}
